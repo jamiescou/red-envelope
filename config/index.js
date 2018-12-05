11fb57024be235708d2bbf6f2a5f5b16222fe37f
zouzhengming <zouzhengming@meiqia.com>
2018-12-05 09:57:02 +0800

    :wrench:

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
-
 module.exports = {
   build: {
     env: require('./prod.env'),
@@ -9,6 +8,7 @@ module.exports = {
     assetsSubDirectory: '',
     assetsPublicPath: '/',
     productionSourceMap: false,
+
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
